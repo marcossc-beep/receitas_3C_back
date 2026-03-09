17a7e4e8c6e18e9f88cd79b949067eec3da6e56d
Professor <Professor@SN-408243.edu.fiesc.com.br>
2026-03-09 11:50:55 -0300

    pa

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ servidor.get('/usuarios', async () => {
 servidor.post('/usuarios', async (request, reply) => {
     const nome = request.body.nome;
     const senha = request.body.senha;
+
+    if (!nome || !senha) {
+        reply.status(400).send({error: 'Nome ou Senha Inválidos!'})
+    }
+
     const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [nome, senha])
     reply.status(201).send({mensagem: "Deu certo!"})
 })
@@ -26,6 +31,23 @@ servidor.post('/usuarios', async (request, reply) => {
 servidor.put('/usuarios/:id', async (request, reply) => {
     const body = request.body;
     const id = request.params.id
+
+    if (!body || !body.nome || !body.senha) {
+        reply.status(400).send({error: 'nome ou senha inv!'})
+    } else if (!id) {
+        reply.status(400).send({error: 'Faltou o ID!'})
+    }
+
+    const existe = await sql.query(
+        'select * from usuario where id = $1', [id]
+    )
+    
+    if (existe.rows.length === 0) {
+        reply.status(400).send(
+            {error: 'Usuário não existe no banco!'}
+        )
+    }
+
     const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])     
     return 'Usuario alterado!'
 })
